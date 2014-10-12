9ef6dd51d37a2f8f5e5f6075d449bf3ab6e098e6
Mikael Sörlin <iamtew@asylunatic.se>
2014-10-12 16:25:32 +0200

    moved the socket open/close to the netpipe function For so few actions the setup/shutdown functions seems overkill

--- irccat.py
+++ irccat.py
@@ -16,31 +16,23 @@ from datetime import datetime
 HOST = '0.0.0.0'
 PORT = 5234
 
-def setup(bot):
+@willie.module.event('001')
+@willie.module.rule('.*')
+def netpipe(bot, trigger):
     """
-    Open listening socket on bot startup
+    Open our listening socket upon successfull server connection when the bot
+    receives RPL_WELCOME (001)
     """
     global sock
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((HOST, PORT))
     sock.listen(5)
 
-
-def shutdown(bot):
-    """
-    Close socket at bot shutdown
-    """
-    sock.close()
-
-
-@willie.module.event('001')
-@willie.module.rule('.*')
-def netpipe(bot, trigger):
     while True:
         conn, addr = sock.accept()
         data = conn.recv(1024)
         logfile = open(os.path.join(bot.config.logdir, 'irccat.log'), 'a')
-        logfile.write(str(datetime.now()) + ' message from ' + addr[0] + ': ' + data)
+        logfile.write(str(datetime.now()) + ' msg ' + addr[0] + ': ' + data)
 
         # First part of the message should be channel/user
         rcpt, msg = data.split(' ', 1)
@@ -60,9 +52,10 @@ def netpipe(bot, trigger):
                 chanlist = bot.config.channels.split(',', 1)
                 bot.msg(chanlist[0], data)
 
-
         if not data:
             break
 
         conn.close()
         logfile.close()
+
+    sock.close()
