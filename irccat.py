fd215ede70ad7f19a5366e8887afa478c7047a54
Mikael Sörlin <iamtew@asylunatic.se>
2014-10-12 19:10:09 +0200

    Socket settings moved from constants to configuration file

--- irccat.py
+++ irccat.py
@@ -9,24 +9,47 @@ https://github.com/iamtew/willie-irccat
 
 import os
 import re
-import willie
 import socket
 from datetime import datetime
+from willie.config import ConfigurationError
+from willie import module
 
-HOST = '0.0.0.0'
-PORT = 5234
+
+def irccat_config(bot):
+    if not bot.config.has_option('irccat', 'address') or \
+       not bot.config.has_option('irccat', 'port'):
+        return False
+    else:
+        return [bot.config.irccat.address, bot.config.irccat.port]
 
 
-@willie.module.event('001')
-@willie.module.rule('.*')
+def configure(config):
+    """
+    | [irccat] | example | purpose        |
+    | -------- | ------- | -------------- |
+    | address  | 0.0.0.0 | Listen address |
+    | port     | 5234    | Listen port    |
+    """
+    if config.option('Listening address and port for irccat', False):
+        config.interactive_add('irccat', 'address', 'Listen address', '0.0.0.0')
+        config.interactive_add('irccat', 'port', 'Listen port', '5234')
+
+
+def setup(bot):
+    if not irccat_config(bot):
+        raise ConfigurationError('irccat module not configured')
+
+
+@module.event('001')
+@module.rule('.*')
 def netpipe(bot, trigger):
     """
     Open our listening socket upon successfull server connection when the bot
     receives RPL_WELCOME (001)
     """
-    global sock
+    netcfg = irccat_config(bot)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind((HOST, PORT))
+    sock.bind((netcfg[0], int(netcfg[1])))
     sock.listen(5)
 
     while True:
